f904d4f509ccda0bd2971979198350cdf17c9794
Shovon Hasan <im.shovonhasan@gmail.com>
2016-09-09 19:01:58 +0000

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,57 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:shovon)
+    @admin = users(:admin)
+    @other_user = users(:invader)
+  end
+  
   test "should get new" do
     get signup_path
     assert_response :success
   end
+  
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+  
+  test "access page while logged in" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+  end
 
+  test "admin attribute should not be editable via the web" do
+    log_in_as(@user)
+    assert_not @user.admin?
+    patch user_path(@user), params: { user: { password: 'password',
+                                              password_confirmation: 'password',
+                                              admin: true } }
+    assert_not @user.admin?
+  end
+  
+  test "should redirect destroy to log in page when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect to home page when not admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+  
+  test "successful delete while logged in as admin" do
+    log_in_as(@admin)
+    assert_difference 'User.count', -1 do
+      delete user_path(@other_user)
+    end
+  end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -4,32 +4,89 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   
   def setup
     @user = users(:shovon)
+    @other_user = users(:invader)
   end
   
-  # test "unsuccessful edit" do
-  #   log_in_as(@user)
-  #   get edit_user_path(@user)
-  #   assert_template 'users/edit'
-  #   patch user_path(@user), params: { user: {name: "", email: "user@invalid", 
-  #                                           password: "foo", password_confirmation: "bar" } }
-  #   assert_template 'users/edit'
-  #   assert_select 'div#error_explanation'
-  #   assert_select 'div.field_with_errors'
-  # end
+  test "successful log_in as other user" do
+    log_in_as(@other_user)
+    get edit_user_path(@other_user)
+    assert_template 'users/edit'
+  end
+  
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: {name: "", email: "user@invalid", 
+                                            password: "foo", password_confirmation: "bar" } }
+    assert_template 'users/edit'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
   
   
-  # test "successful edit" do
-  #   log_in_as(@user)
-  #   get edit_user_path(@user)
-  #   assert_template 'users/edit'
-  #   new_name = "Foo Bar"
-  #   new_email = "foo@bar.com"
-  #   patch user_path(@user), params: { user: {name: new_name, email: new_email,
-  #                                           password: "", password_confirmation: "" } }
-  #   assert_not flash.empty?
-  #   assert_redirected_to @user
-  #   @user.reload
-  #   assert_equal new_name, @user.name
-  #   assert_equal new_email, @user.email
-  # end
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    new_name = "Foo Bar"
+    new_email = "foo@bar.com"
+    patch user_path(@user), params: { user: {name: new_name, email: new_email,
+                                            password: "", password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal new_name, @user.name
+    assert_equal new_email, @user.email
+  end
+  
+  test "should redirect when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect update when not logged in" do
+    get edit_user_path(@user)
+    new_email = "foo@bar.com"
+    new_name = "Foo Bar"
+    patch user_path(@user), params: { user: {name: new_name, email: new_email,
+                                            password: "", password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    new_email = "foo@bar.com"
+    new_name = "Foo Bar"
+    patch user_path(@user), params: { user: {name: new_name, email: new_email,
+                                            password: "", password_confirmation: "" } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                      email: email,
+                                      password:              "",
+                                      password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
