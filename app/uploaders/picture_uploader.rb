6de64a3daeac908cf5e5be16b77cfeb6c5b39fce
Shovon Hasan <im.shovonhasan@gmail.com>
2016-12-20 10:53:43 +0000

    Add support for uploading images with microposs and storing uploaded images on AWS

---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  
+  if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region => ENV[’S3_REGION’]
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+    end
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+end
