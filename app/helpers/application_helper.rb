600976fa0e5e9c8ba48c5ba6f86b5d0d16a7dfcf
Shovon Hasan <im.shovonhasan@gmail.com>
2016-12-31 22:30:28 +0000

    Customize site to brand it as Moonbeam

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   #Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Shovon Hasan's Sample Application"
+    base_title = "Moonbeam"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 end
