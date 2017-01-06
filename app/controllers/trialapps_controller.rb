cba866a52bb8ac68ea7bab1051e54be7558ed846
Shovon Hasan <im.shovonhasan@gmail.com>
2017-01-06 03:35:23 +0000

    Add questions page and appropriate attributes in the Trialapps model

--- app/controllers/trialapps_controller.rb
+++ app/controllers/trialapps_controller.rb
@@ -2,6 +2,7 @@ class TrialappsController < ApplicationController
   before_action :logged_in_user, only: [:new]
   before_action :admin_user,     only: [:destroy]
   
+  
   def new
     @trialapp = Trialapp.new
   end
@@ -17,13 +18,18 @@ class TrialappsController < ApplicationController
   end
   
   def questions
-  
   end
   
   def answers
-    
+    @trialapp = current_user.trialapps.build(trialapps_params)
+    if @trialapp.save
+      flash[:success] = "Information saved!"
+      redirect_to root_url
+    else
+      render 'questions'
+    end
   end
-  
+
   def reject
   end
   
@@ -36,7 +42,9 @@ class TrialappsController < ApplicationController
   private
     def trialapps_params
       params.require(:trialapp).permit(:character_name, 
-                                      :server, :character_class, :spec)
+                                      :server, :character_class, :spec,
+                                      :question_one, :question_two, 
+                                      :question_three, :question_four, )
     end
     
     # Before filters
