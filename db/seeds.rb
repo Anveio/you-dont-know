253e37e9ea7f1f9cc4354b3bf198db2f0d0db00c
Shovon Hasan <im.shovonhasan@gmail.com>
2017-01-12 04:08:04 +0000

    Everything please work

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #            password: "password", password_confirmation: "password", admin: true,
 #            activated: true, activated_at: Time.zone.now, raider: true)
 
-User.create!(name: "Skythan", email: "mamuise@gmail.com", 
-            password: "password", password_confirmation: "password", admin: true,
-            activated: true, activated_at: Time.zone.now, raider: true)
+#User.create!(name: "Skythan", email: "mamuise@gmail.com", 
+#            password: "password", password_confirmation: "password", admin: true,
+#            activated: true, activated_at: Time.zone.now, raider: true)
 
-User.create!(name: "Anveio", email: "ar_thanos@hotmail.com", 
-            password: "gold793a", password_confirmation: "gold793a", admin: true,
-            activated: true, activated_at: Time.zone.now, raider: true)
+#User.create!(name: "Anveio", email: "ar_thanos@hotmail.com", 
+#            password: "gold793a", password_confirmation: "gold793a", admin: true,
+#            activated: true, activated_at: Time.zone.now, raider: true)
 
 User.create!(name: "Pinionz", email: "taliesingaming@gmail.com", 
             password: "password", password_confirmation: "password", admin: true,
